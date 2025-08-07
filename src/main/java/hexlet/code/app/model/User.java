338d54078cce92e5bfce33029469cb60068511dc
Roman2 <rom.kawirshin@gmail.com>
2025-08-07 22:06:18 +0300

    Добавлена модель User

---
+++ src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,38 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+}
