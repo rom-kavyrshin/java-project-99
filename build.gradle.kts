9455a0910de4cdcc269c6b3ca330a8dcfa1cec93
Roman2 <rom.kawirshin@gmail.com>
2025-08-07 22:04:37 +0300

    Добавлены зависимости для jpa

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,10 +41,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	annotationProcessor("org.projectlombok:lombok")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2:2.3.232")
+
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
