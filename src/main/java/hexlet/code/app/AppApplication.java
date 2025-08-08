4aaf5899fcf56d2bb09939b672d85fde1f302820
Roman2 <rom.kawirshin@gmail.com>
2025-08-09 01:09:55 +0300

    Добавлено логгирование профиля

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -1,13 +1,28 @@
 package hexlet.code.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class AppApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(AppApplication.class);
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        logger.info("Active profile " + activeProfile);
     }
 
 }
