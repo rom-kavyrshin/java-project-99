f169d61825f31aee0ba01c68a02fdc8af668005a
Roman2 <rom.kawirshin@gmail.com>
2025-08-11 16:50:09 +0300

    Добавлен testWelcome

--- src/test/java/hexlet/code/app/AppApplicationTests.java
+++ src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,14 +1,25 @@
 package hexlet.code.app;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AppApplicationTests {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testWelcome() {
+        String response = testRestTemplate.getForObject("/welcome", String.class);
+        Assertions.assertEquals("Welcome to Spring", response);
     }
 
 }
