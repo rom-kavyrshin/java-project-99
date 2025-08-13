a2044b8e933a490441533e69f68b01d6a8ee9a43
Roman2 <rom.kawirshin@gmail.com>
2025-08-14 02:54:07 +0300

    Добавлен mapper для user

---
+++ src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package hexlet.code.app.mapper;
+
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class UserMapper {
+    public abstract User map(UserCreateDTO userDTO);
+    public abstract UserDTO map(User user);
+}
