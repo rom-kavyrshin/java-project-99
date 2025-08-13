e4aea48467cdfd1e357eaea14943e73e791861c3
Roman2 <rom.kawirshin@gmail.com>
2025-08-13 19:34:03 +0300

    Добавил аннотацию Repository

--- src/main/java/hexlet/code/app/repositories/UserRepository.java
+++ src/main/java/hexlet/code/app/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package hexlet.code.app.repositories;
 
 import hexlet.code.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
