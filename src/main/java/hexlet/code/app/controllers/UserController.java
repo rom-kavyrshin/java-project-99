63b61569a28327c909e309d01231c335ee41128d
Roman2 <rom.kawirshin@gmail.com>
2025-08-14 16:04:34 +0300

    Добавлены заглушки методов в UserController

--- src/main/java/hexlet/code/app/controllers/UserController.java
+++ src/main/java/hexlet/code/app/controllers/UserController.java
@@ -2,12 +2,16 @@ package hexlet.code.app.controllers;
 
 import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,12 +38,28 @@ public class UserController {
                 .toList();
     }
 
+    @GetMapping(path = "/{id}")
+    public UserDTO show(@PathVariable long id) {
+        throw new RuntimeException();
+    }
+
     @PostMapping(path = "")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO create(@Valid @RequestBody UserCreateDTO userCreateDTO) {
         var user = userMapper.map(userCreateDTO);
         userRepository.save(user);
         return userMapper.map(user);
+    }
+
+    @PutMapping(path = "/{id}")
+    public UserDTO update(@PathVariable long id, @Valid @RequestBody UserUpdateDTO userUpdateDTO) {
+        throw new RuntimeException();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public UserDTO update(@PathVariable long id) {
+        throw new RuntimeException();
     }
 
 }
