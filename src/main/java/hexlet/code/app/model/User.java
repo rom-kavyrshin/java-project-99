3ecab16f32205fd3e182ddc0bd547f6b99a47849
Roman2 <rom.kawirshin@gmail.com>
2025-08-14 03:25:43 +0300

    Добавлена валидация на Entity

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,9 +35,13 @@ public class User {
 
     private String lastName;
 
+    @NotNull
+    @Email
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NotNull
+    @Size(min = 3)
     private String password;
 
     @CreatedDate
