40435b467c6c6b4ee4a0de49d7ac8403e313b107
Roman2 <rom.kawirshin@gmail.com>
2025-08-15 23:20:24 +0300

    Свойства в UserUpdateDTO обернуты в JsonNullable

--- src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,20 +1,25 @@
 package hexlet.code.app.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @Getter
 @Setter
 public class UserUpdateDTO {
-    private String firstName;
 
-    private String lastName;
+    private JsonNullable<String> firstName;
 
+    private JsonNullable<String> lastName;
+
+    @NotNull
     @Email
-    private String email;
+    private JsonNullable<String> email;
 
+    @NotNull
     @Size(min = 3)
-    private String password;
+    private JsonNullable<String> password;
 }
--- src/test/java/hexlet/code/app/ModelGenerator.java
+++ src/test/java/hexlet/code/app/ModelGenerator.java
@@ -8,6 +8,7 @@ import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.instancio.Model;
 import org.instancio.Select;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -31,10 +32,10 @@ public class ModelGenerator {
                 .toModel();
 
         userUpdateDTOModel = Instancio.of(UserUpdateDTO.class)
-                .supply(Select.field(UserUpdateDTO::getFirstName), () -> faker.name().firstName())
-                .supply(Select.field(UserUpdateDTO::getLastName), () -> faker.name().lastName())
-                .supply(Select.field(UserUpdateDTO::getEmail), () -> faker.internet().emailAddress())
-                .supply(Select.field(UserUpdateDTO::getPassword), () -> faker.internet().password())
+                .supply(Select.field(UserUpdateDTO::getFirstName), () -> JsonNullable.of(faker.name().firstName()))
+                .supply(Select.field(UserUpdateDTO::getLastName), () -> JsonNullable.of(faker.name().lastName()))
+                .supply(Select.field(UserUpdateDTO::getEmail), () -> JsonNullable.of(faker.internet().emailAddress()))
+                .supply(Select.field(UserUpdateDTO::getPassword), () -> JsonNullable.of(faker.internet().password()))
                 .toModel();
     }
 }
