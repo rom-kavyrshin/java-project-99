83326aaa6facc7552e156b1bbd5ea7d25e906353
Roman2 <rom.kawirshin@gmail.com>
2025-08-15 23:42:10 +0300

    Доработан objectMapperBuilder чтобы нормально сериализовывались даты

--- src/main/java/hexlet/code/app/config/JacksonConfig.java
+++ src/main/java/hexlet/code/app/config/JacksonConfig.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +14,9 @@ public class JacksonConfig {
     @Bean
     Jackson2ObjectMapperBuilder objectMapperBuilder() {
         var builder = new Jackson2ObjectMapperBuilder();
-        builder.serializationInclusion(JsonInclude.Include.NON_NULL).modulesToInstall(new JsonNullableModule());
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL)
+                .modulesToInstall(new JsonNullableModule(), new JavaTimeModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return builder;
     }
 }
