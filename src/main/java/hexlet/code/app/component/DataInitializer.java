96602f0e7ccecb984fbbd0b28c278940807765b7
Roman2 <rom.kawirshin@gmail.com>
2025-08-22 00:10:01 +0300

    Обновлен DataInitializer

--- src/main/java/hexlet/code/app/component/DataInitializer.java
+++ src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -1,7 +1,9 @@
 package hexlet.code.app.component;
 
 import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.repositories.UserRepository;
 import hexlet.code.app.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -9,17 +11,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataInitializer implements ApplicationRunner {
 
-    private final UserService userService;
+    public static final String DEFAULT_USER_EMAIL = "hexlet@example.com";
+    public static final String DEFAULT_USER_PASSWORD_KEY = "DEFAULT_PASSWORD";
 
-    public DataInitializer(UserService userService) {
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    @Value("#{environment.DEFAULT_PASSWORD}")
+    private String defaultUserPassword;
+
+    public DataInitializer(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var user = new UserCreateDTO();
-        user.setEmail("hexlet@example.com");
-        user.setPassword("qwerty");
-        userService.create(user);
+        if (!userRepository.existsByEmail(DEFAULT_USER_EMAIL)) {
+            var user = new UserCreateDTO();
+            user.setEmail(DEFAULT_USER_EMAIL);
+            user.setPassword(defaultUserPassword);
+            userService.create(user);
+        }
     }
 }
