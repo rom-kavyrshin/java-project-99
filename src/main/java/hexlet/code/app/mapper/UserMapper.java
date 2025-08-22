915c08bd58fe60ce1bbe72f7a63d8698f1be76c3
Roman2 <rom.kawirshin@gmail.com>
2025-08-22 03:10:23 +0300

    Исправлен UserMapper

--- src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -4,11 +4,16 @@ import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.dto.UserDTO;
 import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.model.User;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = { JsonNullableMapper.class },
@@ -17,7 +22,29 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Mapping(source = "password", target = "passwordDigest")
     public abstract User map(UserCreateDTO userDTO);
+
     public abstract UserDTO map(User user);
+
+    @Mapping(source = "password", target = "passwordDigest")
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
+
+    @BeforeMapping
+    public void hashPassword(UserCreateDTO dto) {
+        var password = dto.getPassword();
+        dto.setPassword(passwordEncoder.encode(password));
+    }
+
+    @BeforeMapping
+    public void hashPassword(UserUpdateDTO dto) {
+        if (dto.getPassword().isPresent()) {
+            var password = dto.getPassword().get();
+            dto.setPassword(JsonNullable.of(passwordEncoder.encode(password)));
+        }
+    }
 }
