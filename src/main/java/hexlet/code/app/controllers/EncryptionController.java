c2d1b13f56f5715ed3c1f5675d2356a8b532773a
Roman2 <rom.kawirshin@gmail.com>
2025-09-05 23:39:31 +0300

    Исправление EncryptionController

--- src/main/java/hexlet/code/app/controllers/EncryptionController.java
+++ src/main/java/hexlet/code/app/controllers/EncryptionController.java
@@ -3,7 +3,7 @@ package hexlet.code.app.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
-import org.springframework.util.ResourceUtils;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/encryption")
@@ -20,16 +19,13 @@ public class EncryptionController {
 
     @PostMapping(path = "/encrypt")
     @ResponseStatus(HttpStatus.CREATED)
-    public String encrypt(@RequestParam String password, @RequestParam String salt) throws IOException {
-        var srcFile = ResourceUtils.getFile("classpath:certs/private.pem");
-        String srcPem = Files.readString(srcFile.toPath(), StandardCharsets.UTF_8);
+    public Map<String, String> encrypt(@RequestParam String clearText, @RequestParam String password) throws IOException {
+        String generatedSalt = KeyGenerators.string().generateKey();
 
-//        String generatedSalt = KeyGenerators.string().generateKey();
+        TextEncryptor encryptor = Encryptors.delux(password, generatedSalt);
+        String encryptedPem = encryptor.encrypt(clearText);
 
-        TextEncryptor encryptor = Encryptors.delux(password, salt);
-        String encryptedPem = encryptor.encrypt(srcPem);
-
-        return encryptedPem;
+        return Map.of("result", encryptedPem, "salt", generatedSalt);
     }
 
     @PostMapping(path = "/decrypt")
