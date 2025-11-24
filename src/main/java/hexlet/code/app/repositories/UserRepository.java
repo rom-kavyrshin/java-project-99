8050cbe73263a5116a8e361861f8e25c806927c3
Roman2 <rom.kawirshin@gmail.com>
2025-11-24 08:01:49 +0300

    Добавил BaseJpaRepository

--- src/main/java/hexlet/code/app/repositories/UserRepository.java
+++ src/main/java/hexlet/code/app/repositories/UserRepository.java
@@ -1,13 +1,12 @@
 package hexlet.code.app.repositories;
 
 import hexlet.code.app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends BaseJpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
