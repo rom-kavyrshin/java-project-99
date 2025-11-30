6e73d3cfbf762d9c35862c20bf59c604643c4fbe
Roman2 <rom.kawirshin@gmail.com>
2025-11-30 08:01:31 +0300

    Добавлена связь many to many

--- src/main/java/hexlet/code/app/model/Label.java
+++ src/main/java/hexlet/code/app/model/Label.java
@@ -1,11 +1,14 @@
 package hexlet.code.app.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,6 +19,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "labels")
@@ -34,6 +38,9 @@ public class Label implements BaseEntity {
     @Column(nullable = false, unique = true)
     @Size(min = 3, max = 1000)
     private String name;
+
+    @ManyToMany(mappedBy = "labels", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Task> tasks;
 
     @CreatedDate
     private LocalDateTime createdAt;
--- src/main/java/hexlet/code/app/model/Task.java
+++ src/main/java/hexlet/code/app/model/Task.java
@@ -1,13 +1,17 @@
 package hexlet.code.app.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -21,6 +25,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "tasks")
@@ -55,6 +60,14 @@ public class Task implements BaseEntity {
     @ManyToOne(optional = true)
     @OnDelete(action = OnDeleteAction.RESTRICT)
     private User assignee;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "task_label",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "label_id")
+    )
+    private List<Label> labels;
 
     @CreatedDate
     private LocalDateTime createdAt;
