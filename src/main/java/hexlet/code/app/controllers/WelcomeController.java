e68cec398d05bbb5e2c7f150bfb0ba1fdb731002
Roman2 <rom.kawirshin@gmail.com>
2025-12-16 21:40:03 +0300

    Перемещен WelcomeController

--- src/main/java/hexlet/code/app/controllers/WelcomeController.java
+++ src/main/java/hexlet/code/app/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package hexlet.code.app;
+package hexlet.code.app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
