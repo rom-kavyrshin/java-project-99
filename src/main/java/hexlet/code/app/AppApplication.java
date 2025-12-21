e09bf005b327fd25ea815cdd6533c9fb62bdf658
Roman2 <rom.kawirshin@gmail.com>
2025-12-21 20:59:15 +0300

    refactoring

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -1,7 +1,6 @@
 package hexlet.code.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +8,10 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
 public class AppApplication {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AppApplication.class);
 
     @Value("${spring.profiles.active}")
     private String activeProfile;
@@ -24,7 +22,7 @@ public class AppApplication {
 
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReady() {
-        LOG.info("Active profile {}", activeProfile);
+        log.info("Active profile {}", activeProfile);
     }
 
 }
